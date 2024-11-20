3ede869fdd67a24745fc314a6c4547d689602dde
Ocskó Gábor <ogabor@gmail.com>
2024-11-20 18:48:40 +0100

    Add CORS support to Flask server for backtest results endpoint

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS, cross_origin
 from controllers.backtest_controller import get_backtest_slices
 
 app = Flask(__name__)
-
+cors = CORS(app) 
 
 @app.route('/backtest-results', methods=['GET'])
+@cross_origin()
 def sample():
     results = get_backtest_slices()
     return jsonify(results)
