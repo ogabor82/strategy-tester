57cbc8170110b54b23119af80d33e55f462995f9
Ocskó Gábor <ogabor@gmail.com>
2024-11-21 17:57:33 +0100

    Add configuration controller and endpoint to retrieve configurations

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS, cross_origin
 from controllers.backtest_controller import get_backtest_slices, get_backtest_sessions, get_backtest_slices_by_session_id, get_backtest_slices_by_strategy_id
+from controllers.configuration_controller import get_configurations
 from controllers.strategy_controller import create_strategy, get_strategies
 
 app = Flask(__name__)
@@ -44,6 +45,12 @@ def create_new_strategy():
 def strategy(id):
     sessions = get_backtest_slices_by_strategy_id(id)
     return jsonify([dict(session) for session in sessions])
+
+@app.route('/configurations', methods=['GET'])
+@cross_origin()
+def configurations():
+    configurations = get_configurations()
+    return jsonify(configurations)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
