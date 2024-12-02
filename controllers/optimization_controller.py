62d821005761d2b53323fbb3495225e9b1b19f5f
Ocskó Gábor <ogabor@gmail.com>
2024-12-02 20:21:42 +0100

    Enhance optimization slice retrieval

--- controllers/optimization_controller.py
+++ controllers/optimization_controller.py
@@ -23,6 +23,11 @@ def get_optimization_sessions():
 def get_optimization_slices_by_session_id(id: int):
     init_db()
     cursor = DB.cursor()
-    cursor.execute("SELECT * FROM optimization_slice WHERE optimization_session_id = ?", (id,))
+    cursor.execute("""
+        SELECT *, strategy.name as strategy_name
+        FROM optimization_slice
+        LEFT JOIN strategy ON optimization_slice.strategy_id = strategy.id
+        WHERE optimization_slice.optimization_session_id = ?
+    """, (id,))
     rows = cursor.fetchall()
     return [dict(row) for row in rows]
