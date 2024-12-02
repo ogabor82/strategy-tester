90c1258524f5db7fe8ae428d5ad8e39aa646c0de
Ocskó Gábor <ogabor@gmail.com>
2024-12-02 10:54:24 +0100

    Add SeriousMACD strategy support: Introduce backtest sets for SeriousMACD in a new module and update db_seed.py to reference these sets.

---
+++ strategies/SeriousMACD/SeriousMACD_db.py
@@ -0,0 +1,19 @@
+backtest_sets_serious_macd = [{
+    "name": "default",
+    "macd_a": 85,
+    "macd_b": 80,
+    "stoch_k": 70,
+    "stoch_k_smoothing": 10,
+    "stoch_d_smoothing": 3,
+    "ema": 200,
+    "sl": 0.82
+}, {
+    "name": "slow",
+    "macd_a": 85,
+    "macd_b": 80,
+    "stoch_k": 70,
+    "stoch_k_smoothing": 10,
+    "stoch_d_smoothing": 3,
+    "ema": 200,
+    "sl": 0.82
+}]
