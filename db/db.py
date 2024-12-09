3b618230b9205bc67f12f95b8c915d4f6db00ecd
Ocskó Gábor <ogabor@gmail.com>
2024-12-09 17:29:07 +0100

    Implement project management features

--- db/db.py
+++ db/db.py
@@ -3,14 +3,16 @@ from db.db_seed import strategies, timeframe_sets, timeframes
 
 DB = None
 
+
 def init_db():
     global DB
     try:
-        DB = sqlite3.connect('./strategy_tester.db')
+        DB = sqlite3.connect("./strategy_tester.db")
         create_tables()
     except sqlite3.Error as e:
         print(f"Error connecting to database: {e}")
         return e
+
 
 def create_tables():
     if DB is None:
@@ -19,18 +21,29 @@ def create_tables():
 
     try:
         cursor = DB.cursor()
-        
+
+        # Create the "project" table
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS "project" (
+        "id" INTEGER PRIMARY KEY,
+        "name" VARCHAR,
+        "goal" VARCHAR,
+        "details" VARCHAR
+        );
+        """)
+
         # Create the "backtest_session" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "backtest_session" (
         "id" INTEGER PRIMARY KEY,
+        "project_id" INTEGER,
         "name" VARCHAR,
         "details" VARCHAR
         );
-        ''')
+        """)
 
         # Create the "backtest_slice" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "backtest_slice" (
         "id" INTEGER PRIMARY KEY,
         "backtest_session_id" INTEGER,
@@ -50,20 +63,20 @@ def create_tables():
         "kelly_criterion" FLOAT,
         "filename" VARCHAR
         );
-        ''')       
-
+        """)
 
         # Create the "optimization_session" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "optimization_session" (
         "id" INTEGER PRIMARY KEY,
+        "project_id" INTEGER,
         "name" VARCHAR,
         "details" VARCHAR
         );
-        ''')
+        """)
 
         # Create the "optimization_slice" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "optimization_slice" (
         "id" INTEGER PRIMARY KEY,
         "optimization_session_id" INTEGER,
@@ -75,23 +88,26 @@ def create_tables():
         "interval" VARCHAR,
         "optimization_results" VARCHAR
         );
-        ''')
+        """)
 
         # Create the "timeframe_set" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "timeframe_set" (
         "id" INTEGER PRIMARY KEY,
         "name" VARCHAR
         );
-        ''')
+        """)
 
         # Insert seed data into the timeframe_set table
-        cursor.executemany('''
+        cursor.executemany(
+            """
         INSERT INTO timeframe_set (id, name) VALUES (?, ?)
-        ''', timeframe_sets)
+        """,
+            timeframe_sets,
+        )
 
         # Create the "timeframe" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "timeframe" (
         "id" INTEGER PRIMARY KEY,
         "timeframe_set_id" INTEGER,
@@ -100,16 +116,18 @@ def create_tables():
         "end" DATETIME,
         "interval" VARCHAR
         );
-        ''')
+        """)
 
         # Insert seed data into the timeframe table
-        cursor.executemany('''
+        cursor.executemany(
+            """
         INSERT INTO timeframe (id, timeframe_set_id, name, start, end, interval) VALUES (?, ?, ?, ?, ?, ?)
-        ''', timeframes)
-
+        """,
+            timeframes,
+        )
 
         # Create the "strategy" table
-        cursor.execute('''
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS "strategy" (
         "id" INTEGER PRIMARY KEY,
         "name" VARCHAR,
@@ -117,23 +135,28 @@ def create_tables():
         "backtest_sets" VARCHAR,
         "optimization_sets" VARCHAR
         );
-        ''')     
+        """)
 
         # Insert seed data into the strategy table
-        cursor.executemany('''
+        cursor.executemany(
+            """
         INSERT INTO strategy (id, name, description, backtest_sets, optimization_sets) VALUES (?, ?, ?, ?, ?)
-        ''', strategies)   
+        """,
+            strategies,
+        )
 
         DB.commit()
     except sqlite3.Error as e:
         print(f"Error creating table: {e}")
         return e
-    
+
+
 def load_last_session():
     global DB
     cursor = DB.cursor()
     cursor.execute("SELECT * FROM backtest_session ORDER BY id DESC LIMIT 1")
     return cursor.fetchone()
+
 
 def load_last_optimization_session():
     global DB
--- server.py
+++ server.py
@@ -1,91 +1,121 @@
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS, cross_origin
 from backtest import run_backtest
-from controllers.backtest_controller import get_backtest_slices, get_backtest_sessions, get_backtest_slices_by_session_id, get_backtest_slices_by_strategy_id
-from controllers.optimization_controller import get_optimization_slices_by_session_id, get_optimization_sessions
-from controllers.timeframe_set_controller import get_timeframe_sets, get_timeframe_sets_with_timeframes
+from controllers.backtest_controller import (
+    get_backtest_slices,
+    get_backtest_sessions,
+    get_backtest_slices_by_session_id,
+    get_backtest_slices_by_strategy_id,
+)
+from controllers.optimization_controller import (
+    get_optimization_slices_by_session_id,
+    get_optimization_sessions,
+)
+from controllers.project_controller import create_project, delete_project, get_projects
+from controllers.timeframe_set_controller import (
+    get_timeframe_sets,
+    get_timeframe_sets_with_timeframes,
+)
 from controllers.strategy_controller import create_strategy, get_strategies
 from controllers.ticker_set_controller import get_ticker_sets
-from create_optimization_session import delete_optimization_session, save_optimization_session
-from create_session import delete_backtest_session, save_session as save_backtest_session
+from create_optimization_session import (
+    delete_optimization_session,
+    save_optimization_session,
+)
+from create_session import (
+    delete_backtest_session,
+    save_session as save_backtest_session,
+)
 from optimization import run_optimization
 
 # app = Flask(__name__)
-app = Flask(__name__, static_folder='reports')
+app = Flask(__name__, static_folder="reports")
 
-cors = CORS(app) 
+cors = CORS(app)
 
-@app.route('/backtest-results', methods=['GET'])
+
+@app.route("/backtest-results", methods=["GET"])
 @cross_origin()
 def sample():
     results = get_backtest_slices()
     return jsonify(results)
 
-@app.route('/backtest-sessions', methods=['GET'])
+
+@app.route("/backtest-sessions", methods=["GET"])
 @cross_origin()
 def backtest_sessions():
     sessions = get_backtest_sessions()
     return jsonify([dict(session) for session in sessions])
 
-@app.route('/backtest-sessions', methods=['POST'])
+
+@app.route("/backtest-sessions", methods=["POST"])
 @cross_origin()
 def create_backtest_session():
     data = request.get_json()
     result = save_backtest_session(data["name"], data["details"])
     return jsonify([dict(result)][0])
 
-@app.route('/backtest-sessions/<int:id>', methods=['GET'])
+
+@app.route("/backtest-sessions/<int:id>", methods=["GET"])
 @cross_origin()
-def backtest_session(id):    
+def backtest_session(id):
     sessions = get_backtest_slices_by_session_id(id)
     return jsonify([dict(session) for session in sessions])
 
-@app.route('/backtest-sessions/<int:id>', methods=['DELETE'])
+
+@app.route("/backtest-sessions/<int:id>", methods=["DELETE"])
 @cross_origin()
 def delete_backtest_session_server(id):
     delete_backtest_session(id)
     return jsonify({"message": "Backtest session deleted"})
 
-@app.route('/strategies', methods=['GET'])
+
+@app.route("/strategies", methods=["GET"])
 @cross_origin()
 def strategies():
     strategies = get_strategies()
     return jsonify(strategies)
 
-@app.route('/strategies', methods=['POST'])
+
+@app.route("/strategies", methods=["POST"])
 @cross_origin()
 def create_new_strategy():
     data = request.get_json()
-    name = data.get('name')
-    description = data.get('description')
+    name = data.get("name")
+    description = data.get("description")
     create_strategy(name, description)
     return jsonify({"message": "Strategy created"})
 
-@app.route('/strategies/<int:id>', methods=['GET'])
+
+@app.route("/strategies/<int:id>", methods=["GET"])
 @cross_origin()
 def strategy(id):
     sessions = get_backtest_slices_by_strategy_id(id)
     return jsonify([dict(session) for session in sessions])
 
-@app.route('/timeframe-sets', methods=['GET'])
+
+@app.route("/timeframe-sets", methods=["GET"])
 @cross_origin()
 def timeframe_sets():
     timeframe_sets = get_timeframe_sets()
     return jsonify(timeframe_sets)
 
-@app.route('/timeframe-sets-with-timeframes', methods=['GET'])
+
+@app.route("/timeframe-sets-with-timeframes", methods=["GET"])
 @cross_origin()
 def timeframe_sets_with_timeframes():
     timeframe_sets = get_timeframe_sets_with_timeframes()
     return jsonify(timeframe_sets)
 
-@app.route('/optimization-sessions', methods=['GET'])
+
+@app.route("/optimization-sessions", methods=["GET"])
 @cross_origin()
 def optimization_sessions():
     optimization_sessions = get_optimization_sessions()
     return jsonify(optimization_sessions)
 
-@app.route('/optimization-sessions', methods=['POST'])
+
+@app.route("/optimization-sessions", methods=["POST"])
 @cross_origin()
 def create_optimization_session():
     data = request.get_json()
@@ -94,37 +124,65 @@ def create_optimization_session():
     result = save_optimization_session(name, details)
     return jsonify([dict(result)][0])
 
-@app.route('/optimization-sessions/<int:id>', methods=['GET'])
+
+@app.route("/optimization-sessions/<int:id>", methods=["GET"])
 @cross_origin()
 def optimization_session_slices(id):
     optimization_slices = get_optimization_slices_by_session_id(id)
     return jsonify(optimization_slices)
 
-@app.route('/optimization-sessions/<int:id>', methods=['DELETE'])
+
+@app.route("/optimization-sessions/<int:id>", methods=["DELETE"])
 @cross_origin()
 def delete_optimization_session_server(id):
     delete_optimization_session(id)
     return jsonify({"message": "Optimization session deleted"})
 
-@app.route('/ticker-sets', methods=['GET'])
+
+@app.route("/ticker-sets", methods=["GET"])
 @cross_origin()
 def ticker_sets():
     ticker_sets = get_ticker_sets()
     return jsonify(ticker_sets)
 
-@app.route('/run-backtest', methods=['POST'])
+
+@app.route("/run-backtest", methods=["POST"])
 @cross_origin()
 def run_backtest_server():
     data = request.get_json()
     run_backtest(data)
     return jsonify(data)
 
-@app.route('/run-optimization', methods=['POST'])
+
+@app.route("/run-optimization", methods=["POST"])
 @cross_origin()
 def run_optimization_server():
     data = request.get_json()
     run_optimization(data)
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+
+@app.route("/projects", methods=["GET"])
+@cross_origin()
+def projects():
+    projects = get_projects()
+    return jsonify(projects)
+
+
+@app.route("/projects", methods=["POST"])
+@cross_origin()
+def create_project_server():
+    data = request.get_json()
+    result = create_project(data["name"], data["goal"], data["details"])
+    return jsonify([dict(result)][0])
+
+
+@app.route("/projects/<int:id>", methods=["DELETE"])
+@cross_origin()
+def delete_project_server(id):
+    delete_project(id)
+    return jsonify({"message": "Project deleted"})
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
